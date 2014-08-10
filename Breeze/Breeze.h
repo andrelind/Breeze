c894c1c1c4ab60eee9cd2b48896beb3171b59115
André Lind <andre.lind@prisjakt.nu>
2014-08-10 06:33:30 +0200

    Initial Commit

---
+++ Breeze/Breeze.h
@@ -0,0 +1,19 @@
+//
+//  Breeze.h
+//  Breeze
+//
+//  Created by André Lind on 2014-08-10.
+//  Copyright (c) 2014 André Lind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Breeze.
+FOUNDATION_EXPORT double BreezeVersionNumber;
+
+//! Project version string for Breeze.
+FOUNDATION_EXPORT const unsigned char BreezeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Breeze/PublicHeader.h>
+
+
